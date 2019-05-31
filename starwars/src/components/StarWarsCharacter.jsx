b4a84b1c8ba13e9f5640f91de32c243a38ebce5b
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-31 10:04:40 +0100

    Add card covers and hover for each card

--- starwars/src/components/StarWarsCharacter.jsx
+++ starwars/src/components/StarWarsCharacter.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './StarWars.css';
 
-export default function StarWarsCharacter ({name, height, mass, gender, birthYear,}) {
-
-  return (
-    <div className="starWarsCharacter">
-    <h3>{name}</h3>
-   
-    <div>Birth Year: {birthYear}</div>
-    <div>Height: {height}</div>
-    <div>Mass: {mass}</div>
-    <div>Gender: {gender}</div>
-    </div>
-  )
+export default function StarWarsCharacter({
+	name,
+	height,
+	mass,
+	gender,
+	birthYear
+}) {
+	return (
+		<div className="starWarsCharacter">
+			<div className="charNameCard">
+				<div className="charName">{name}</div>
+			</div>
+			<h3>{name}</h3>
+			<div>Birth Year: {birthYear}</div>
+			<div>Height: {height}</div>
+			<div>Mass: {mass}</div>
+			<div>Gender: {gender}</div>
+		</div>
+	);
 }
