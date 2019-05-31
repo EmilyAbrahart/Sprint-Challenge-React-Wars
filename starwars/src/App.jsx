7b1338e9ba5056fc61f14f5edf0c0fdd0cf27fd8
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-31 10:19:27 +0100

    Added extra API endpoints for additional data

--- starwars/src/App.jsx
+++ starwars/src/App.jsx
@@ -6,18 +6,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      starwarsSpecies: [],
+      starwarsHomeWorld: [],
     };
   }
 
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people/');
+    this.getHomeWorld('https://swapi.co/api/planets/');
+    this.getSpecies('https://swapi.co/api/species/');
   }
 
   getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
         return res.json();
@@ -29,6 +30,33 @@ class App extends Component {
         throw new Error(err);
       });
   };
+
+  getHomeWorld = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ starwarsHomeWorld: data.results });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  getSpecies = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ starwarsSpecies: data.results });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+  
 
   render() {
     return (
